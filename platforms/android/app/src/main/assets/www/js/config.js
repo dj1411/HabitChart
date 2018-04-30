93156e407a79ba48c84b6b391f56b1184786c1a1
Jayanta Debnath <jayanta.dn@gmail.com>
2018-04-30 23:48:41 +0530

    added icons

---
+++ platforms/android/app/src/main/assets/www/js/config.js
@@ -0,0 +1,16 @@
+﻿const color1 = "#5D9CBB"; /* darkest */
+const color2 = "#b7d7e8";
+const color3 = "#cfe0e8";
+const color4 = "#F7FBFA"; /* lightest */
+
+const MS_IN_DAY = 86400000;
+const MAX_DATA_DISPLAY = 7;
+const SYNC_INTERVAL_S = 30;
+
+const CONNECTION_JSON_ID = "d55un";
+const MASTER_JSON_ID = "mlerr";
+const TEST_JSON_ID = "14gl63";
+const ACTIVE_JSON_ID = MASTER_JSON_ID;    /* if you change this, you have to reset the data */
+
+const HABIT_COL_WIDTH = 200;
+const DATA_COL_WIDTH = 50;
--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,173 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+function testcode()
+{
+    data = JSON.parse(localStorage.getItem("data"));
+    if (data == null)
+        data = dataInit;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+    data.Timestamp = "";
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    DataSave(0);
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function refreshTable()
+{
+	/* check if mobile screen is too small */
+	if( window.innerWidth < (HABIT_COL_WIDTH + DATA_COL_WIDTH) )
+	{
+		alert( "Your screen is too small for the app to run properly" );
+		return;
+	}
+	
+    var table = document.getElementById("tableMain");
 
-        console.log('Received Event: ' + id);
+    /* delete existing rows */
+    var cntRows = table.rows.length;
+    for (var row = 0; row < cntRows; row++)
+        table.deleteRow(0);
+
+    /* Fill the table */
+    var numDataCol = (window.innerWidth - HABIT_COL_WIDTH) / DATA_COL_WIDTH;
+    for (var r = 0; r <= data.HabitList.length; r++)
+    {
+        var row = table.insertRow(r);
+        var cell = row.insertCell(0);
+        cell.style.width = HABIT_COL_WIDTH + "px";
+
+        /* date row */
+        if (r == 0)     /* The first cell is blank by design */
+        {
+            for (var c = 0; (c < DataListSize()) && (c < numDataCol); c++)
+            {
+                cell = row.insertCell(c + 1);
+                cell.style.width = DATA_COL_WIDTH + "px";
+
+                var date = new Date();
+                date.setDate(date.getDate() - c);
+                if (date.toLocaleDateString().split("/")[0] == date.getDate())  /* check locale setting */
+                    cell.innerHTML = "<div class='th thtop'>" + date.getDate() + "/" + (date.getMonth() + 1) + "</div>";
+                else
+                    cell.innerHTML = "<div class='th thtop'>" + (date.getMonth() + 1) + "/" + date.getDate() + "</div>";
+            }
+        }
+        else
+        {
+            /* Habits */
+            var habit = document.createElement("div");
+            habit.setAttribute("class", "habitColumn");
+            habit.setAttribute("id", "HabitList_" + (r - 1));
+            habit.setAttribute("name", "HabitList_" + (r - 1));
+            habit.setAttribute("onclick", "selectHabit('HabitList_" + (r - 1) + "')");
+            //habit.setAttribute("onclick", "selectHabit('xx')");
+            habit.textContent = data.HabitList[r - 1];
+            cell.appendChild(habit);
+
+            /* Checkboxes */
+            for (var c = 0; (c < DataListSize()) && (c < numDataCol); c++) {
+                cell = row.insertCell(c + 1);
+                cell.style.width = DATA_COL_WIDTH + "px";
+
+                checkbox = document.createElement("input");
+                checkbox.setAttribute("type", "checkbox");
+                checkbox.setAttribute("class", "td");
+
+                var date = new Date();
+                date.setDate(date.getDate() - c);
+                var name = "checkbox_" + (r - 1) + "_" + date.getDate() + "_" + (date.getMonth()+1);
+                checkbox.setAttribute("id", name);
+                checkbox.setAttribute("name", name);
+                checkbox.setAttribute("onclick", "checkboxChange('" + name + "')");
+
+                var date = new Date();
+                date.setDate(date.getDate() - c);
+                if (DataGetByDate(date)[r - 1])
+                    checkbox.checked = true;
+
+                cell.appendChild(checkbox);
+            }
+        }
     }
-};
+}
 
-app.initialize();
+function checkboxChange(name)
+{
+    var idHabit = name.split("_")[1];
+
+    var date = new Date();
+    date.setMonth(parseInt(name.split("_")[3]) -1);
+    date.setDate(name.split("_")[2]);
+
+    var stCheck = document.getElementById(name).checked;
+
+    var arr = DataGetByDate(date);
+    arr[idHabit] = (stCheck) ? 1 : 0;
+    DataSetByDate(date, arr);
+}
+
+function selectHabit(habitId)
+{
+    var selectedHabitId = DataSelectedHabitGetId();
+
+    if (selectedHabitId != null)
+        document.getElementById(selectedHabitId).style.backgroundColor = color4;
+
+    if (habitId == selectedHabitId) /* de-selection */
+    {
+        /* reset selection */
+        DataSelectedHabitUpdate("");
+
+        /* change toolbar icons */
+        toggleToolbar();
+    }
+    else  /* selection */
+    {
+        /* set selection */
+        document.getElementById(habitId).style.backgroundColor = color2; 
+        DataSelectedHabitUpdate(habitId);
+
+        /* change toolbar icons */
+        toggleToolbar();
+    }
+}
+
+function removeHabit()
+{
+    if (DataSelectedHabitGetId() == null)
+        return;
+
+    if (window.confirm( "Are you sure to delete " + DataSelectedHabitGetStr() + "?" ))
+        DataHabitRemove(DataSelectedHabitGetStr());
+
+    refreshTable();
+    DataSelectedHabitUpdate("");
+    toggleToolbar();
+}
+
+function toggleToolbar()
+{
+    if (selectedHabit == "")
+    {
+        $("#divToolbarSelection").css("display", "none");
+        $("#divToolbarNormal").css("display", "block");
+    }
+    else
+    {
+        $("#divToolbarSelection").css("display", "block");
+        $("#divToolbarNormal").css("display", "none");
+    }
+}
+
+function onclickMenu()
+{
+    return; // disable untill any of the menu function is implemented properly
+
+    if ($("#divMenu").css("display") == "block")
+        $("#divMenu").css("display", "none");
+    else
+        $("#divMenu").css("display", "block");
+}
+
+function exitApp()
+{
+    //self.close();
+    navigator.app.exitApp();
+}
