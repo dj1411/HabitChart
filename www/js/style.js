2622c055749c29dc87ff0ebbe697ddfa6688f441
Jayanta Debnath <jayanta.dn@gmail.com>
2018-05-26 22:53:50 +0530

    sidebar header details taken from config

--- www/js/style.js
+++ www/js/style.js
@@ -3,7 +3,10 @@
     /* set the app name */
     document.title = APP_NAME;
     document.getElementById("title").innerText = APP_NAME;
-    document.getElementById("titleSidebar").innerText = data.UserID;
+    
+    /* sidebar header */
+    if(config.name != "") document.getElementById("labelName").innerText = config.name;
+    if(config.email != "") document.getElementById("labelEmail").innerText = config.email;
     
     /* hide the selection toolbar */
     $(".divToolbarSelection").css("display", "none");
