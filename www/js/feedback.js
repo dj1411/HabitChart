0d432053c57b278a78a9f8aacddbe3507abf5bf6
Jayanta Debnath <jayanta.dn@gmail.com>
2018-05-27 15:46:25 +0530

    feedback uploaded

--- www/js/feedback.js
+++ www/js/feedback.js
@@ -1,10 +1,10 @@
-var msg = new Object();
-
 function main() {
     ConfigLoad();
 }
 
 function onsubmitFeedback() {
+    var msg = new Object();
+    
     /* set the user name */
     if(config.name == "") 
         msg.name = "Anonymous";
@@ -20,15 +20,34 @@ function onsubmitFeedback() {
     /* set the message */
     msg.message = document.getElementById("textMessage").value;
     
-    sendFeedback();
-//    window.history.back();
+    sendFeedback(msg);
 }
 
-function sendFeedback() {
+function sendFeedback(msg) {
     if (navigator.onLine) { // check for internet connection
-        $.get("https://api.myjson.com/bins/" + JSONID_FEEDBACK, function sendFeedback_step3(data, status, xhr) { // receive previous data
-            var msglist = data.list;
-            msglist.push(msg);
+        $.get("https://api.myjson.com/bins/" + JSONID_FEEDBACK, function (data, status, xhr) { // receive previous data
+            if(status == "success") {
+                data.list.push(msg);
+                $.ajax({
+                    url: "https://api.myjson.com/bins/" + JSONID_FEEDBACK,
+                    type: "PUT",
+                    data: JSON.stringify(data),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (result, status, xhdr) {
+                        console.log("uploaded");
+                    },
+                    error: function (xhdr, status, msg) {
+                        console.log("upload failed");
+                    }
+                }); 
+            }
+            else {
+                window.history.back();
+            }
         });
+    }
+    else {
+        window.history.back();
     }
 }
