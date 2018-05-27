471007fa7e92cd11094bf008f8b9304d779f898e
Jayanta Debnath <jayanta.dn@gmail.com>
2018-05-27 13:06:51 +0530

    submit button working

--- www/js/feedback.js
+++ www/js/feedback.js
@@ -1,5 +1,28 @@
 function main() {
+    ConfigLoad();
 }
 
 function onsubmitFeedback() {
+    var mes = new Object();
+    
+    /* set the user name */
+    if(config.name == "") 
+        mes.name = "Anonymous";
+    else
+        mes.name = config.name;
+    
+    /* set email */
+    if(config.email == "") 
+        mes.email = "Anonym@ous.com";
+    else
+        mes.email = config.email;
+    
+    /* set the message */
+    mes.message = document.getElementById("textMessage").value;
+    
+    sendFeedback(mes);
+    window.history.back();
+}
+
+function sendFeedback(mes) {
 }
