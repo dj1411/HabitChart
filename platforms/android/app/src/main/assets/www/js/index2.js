d5d14915d153513ba2df2610e1385f8436d773a5
Jayanta Debnath <jayanta.dn@gmail.com>
2018-06-24 18:24:26 +0200

    Android build

--- platforms/android/app/src/main/assets/www/js/index2.js
+++ platforms/android/app/src/main/assets/www/js/index2.js
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2018 Jayanta Debnath
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+
 function main()
 {
 //    DataReset(1, 1, 0); return; /* 0=NA, 1=reset; ramData, localData, cloudData */
@@ -130,15 +154,9 @@ function refreshTable()
             else if(arrData.length == 1) {
                 height = HEIGHT_ROW;
             }
-            else if(curData <= min) {
-                height = 1;
-            }
-            else if(curData >= max) {
-                height = HEIGHT_ROW;
-            }
             else {
-                var step = HEIGHT_ROW / (max - min);
-                height = min + curData*step;
+                var step = HEIGHT_ROW / max;
+                height = curData*step;
             }
             cell.style.borderBottom = height + "px solid";
         }
